b3ee01a02be5134d219fe320c74cd968d9e9d9f5
tikrack <tikrackcode@gmail.com>
2025-04-30 23:10:31 +0330

    feat(handle): handle idnex

--- app/Routes/index.tsx
+++ app/Routes/index.tsx
@@ -1,6 +1,12 @@
-const index = () => {
-  return <>
+import toast, {Toaster} from "react-hot-toast";
 
+const index = () => {
+  const handleToast = () => {
+    toast.success("سلام من به تو")
+  }
+
+  return <>
+    <button onClick={handleToast}>toast</button>
   </>;
 };
 
