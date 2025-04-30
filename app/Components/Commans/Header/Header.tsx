b3424e9e1953d846ce06d36d23a8256807850c89
tikrack <tikrackcode@gmail.com>
2025-05-01 01:43:03 +0330

    feat(logo)

--- app/Components/Commans/Header/Header.tsx
+++ app/Components/Commans/Header/Header.tsx
@@ -1,13 +1,11 @@
 import Image from "~/Components/Commans/UiParts/Image";
+import Logo from "~/Components/Commans/Header/Logo";
 
 const Header = () => {
   return (
     <>
       <header className={"border h-24 flex justify-between px-10 items-center"}>
-        <div className={"flex justify-center items-center gap-2"}>
-          <h2 className={"text-3xl font-medium"}>Dastto</h2>
-          <Image src={"/Images/dastto-logo-1.svg"} className={"w-12"} />
-        </div>
+        <Logo />
       </header>
     </>
   );
---
+++ app/Components/Commans/Header/Logo.tsx
@@ -0,0 +1,15 @@
+import Image from "~/Components/Commans/UiParts/Image";
+import { Link } from "react-router";
+
+const Logo = () => {
+  return (
+    <>
+      <Link to={"/"} className={"flex justify-center items-center gap-2"}>
+        <h2 className={"text-3xl font-medium"}>Dastto</h2>
+        <Image src={"/Images/dastto-logo-1.svg"} className={"w-12"} />
+      </Link>
+    </>
+  );
+};
+
+export default Logo;
