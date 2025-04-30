79703509bd53031f0ebffce72e2df42d6b4116be
tikrack <tikrackcode@gmail.com>
2025-05-01 02:56:22 +0330

    const Divider

--- app/Components/Commans/Header/Header.tsx
+++ app/Components/Commans/Header/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
       <header className={"h-24 flex justify-between px-10 items-center"}>
         <div className={"flex justify-center items-center gap-4"}>
           <Logo />
-          <span className={"h-10 w-0.5 bg-gray-200"}></span>
+          <Divider />
           <Nav />
         </div>
       </header>
---
+++ app/Components/Commans/UiParts/Divider.tsx
@@ -0,0 +1,3 @@
+const Divider = () => <span className="h-10 w-0.5 bg-gray-200" />;
+
+export default Divider;
