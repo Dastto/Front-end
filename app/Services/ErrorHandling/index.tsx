b4aba9af890ccda551099e3c26cad7f82e5c0080
tikrack <tikrackcode@gmail.com>
2025-04-30 23:20:08 +0330

    feat(handle): handle starter

--- app/Services/ErrorHandling/index.tsx
+++ app/Services/ErrorHandling/index.tsx
@@ -1,9 +1,9 @@
 import type {Route} from "../../../.react-router/types/app/+types/root";
 
-const Index = ({error}: Route.ErrorBoundaryProps) => {
+const ErrorHandling = ({error}: Route.ErrorBoundaryProps) => {
     return (<>
         hellos
     </>)
 }
 
-export default Index;
+export default ErrorHandling;
--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout } from "@react-router/dev/routes";
 
 export default [
-    index("./Routes/index.tsx"),
+    layout("./Services/StarterHandling/index.tsx", [
+        index("./Routes/index.tsx"),
+    ])
 ] satisfies RouteConfig;
