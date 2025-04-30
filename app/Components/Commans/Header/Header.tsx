c94a7dc1d9a67e03e850ab87f804ebef767c6fd9
tikrack <tikrackcode@gmail.com>
2025-05-01 01:50:56 +0330

    feat(nav): add nav components

--- app/Components/Commans/Header/Header.tsx
+++ app/Components/Commans/Header/Header.tsx
@@ -1,11 +1,13 @@
-import Image from "~/Components/Commans/UiParts/Image";
 import Logo from "~/Components/Commans/Header/Logo";
 
 const Header = () => {
   return (
     <>
       <header className={"border h-24 flex justify-between px-10 items-center"}>
-        <Logo />
+        <div className={"flex justify-center items-center gap-4"}>
+          <Logo />
+          <span className={"h-10 w-0.5 bg-gray-200"}></span>
+        </div>
       </header>
     </>
   );
