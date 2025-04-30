da6b42ca24254336e95256e6fa3d751a986a8d6f
tikrack <tikrackcode@gmail.com>
2025-05-01 01:03:36 +0330

    feat(handle): handle default dashboard

---
+++ app/Components/Layouts/DefaultLayout.tsx
@@ -0,0 +1,9 @@
+import {Outlet} from "react-router";
+
+const DefaultLayout = () => {
+    return (<>
+        <Outlet />
+    </>)
+}
+
+export default DefaultLayout
--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,9 @@
-import { type RouteConfig, index, layout } from "@react-router/dev/routes";
+import { index, layout, type RouteConfig } from "@react-router/dev/routes";
 
 export default [
-    layout("./Services/StarterHandling/index.tsx", [
-        index("./Routes/index.tsx"),
-    ])
+  layout("./Services/StarterHandling/index.tsx", [
+    layout("./Components/Layouts/DefaultLayout.tsx", [
+      index("./Routes/index.tsx"),
+    ]),
+  ]),
 ] satisfies RouteConfig;
