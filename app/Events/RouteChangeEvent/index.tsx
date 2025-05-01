48ef2bcdae9063f5a1d244547a66165e44e6c9fb
tikrack <tikrackcode@gmail.com>
2025-05-01 22:57:35 +0330

    feat(events): handle events

--- app/Events/RouteChangeEvent/index.tsx
+++ app/Events/RouteChangeEvent/index.tsx
@@ -1,5 +1,24 @@
+import { useEffect, useRef } from "react";
+import LoadingBar, { type LoadingBarRef } from "react-top-loading-bar";
+import { useLocation } from "react-router";
+
 const RouteChangeEvent = () => {
-  return <></>;
+  const ref = useRef<LoadingBarRef>(null);
+  const location = useLocation();
+
+  ref.current?.start("continuous", 50);
+
+  useEffect(() => {
+    setTimeout(() => {
+      ref.current?.complete();
+    }, 100);
+  }, [location]);
+
+  return (
+    <div className={"dir-ltr"}>
+      <LoadingBar ref={ref} color={"#3E7BFF"} />
+    </div>
+  );
 };
 
 export default RouteChangeEvent;
