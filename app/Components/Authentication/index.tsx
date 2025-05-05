dbd73992b36719f210ebba4218c1cebe8e21a083
tikrack <tikrackcode@gmail.com>
2025-05-05 22:02:08 +0330

    feat(corner): add corner button

--- app/Components/Authentication/index.tsx
+++ app/Components/Authentication/index.tsx
@@ -1,4 +1,5 @@
 import { Clipboard } from "iconsax-reactjs";
+import CornerButton from "~/Components/Commans/UiParts/CornerButton";
 
 const Authentication = () => {
   return (
@@ -8,7 +9,7 @@ const Authentication = () => {
           "h-[calc(100dvh-96px)] w-full flex justify-center gap-32 pb-5"
         }
       >
-        <div className="basis-1/2"></div>
+        <div className="basis-1/2 flex justify-center items-center"></div>
         <div className="basis-1/2">
           <div className={"relative h-full rounded-4xl overflow-hidden"}>
             <img
@@ -17,14 +18,10 @@ const Authentication = () => {
               loading={"lazy"}
               className={"h-full object-cover"}
             />
-            <div
-              className={
-                "bg-black/60 absolute bottom-5 flex-row-reverse gap-1.5 right-5 rounded-[55px] backdrop-blur-[5px] text-[18px] p-3 px-4 flex justify-center items-center text-white"
-              }
-            >
+            <CornerButton>
               ممنونم از Uiarshi
               <Clipboard className={"track-2"} variant={"TwoTone"} />
-            </div>
+            </CornerButton>
           </div>
         </div>
       </main>
