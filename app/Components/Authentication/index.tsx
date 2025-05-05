67ddda6ded40dcbc412db922be3a0782f844d1e2
tikrack <tikrackcode@gmail.com>
2025-05-05 21:06:28 +0330

    feat(office): handle and add office picture

--- app/Components/Authentication/index.tsx
+++ app/Components/Authentication/index.tsx
@@ -1,5 +1,12 @@
 const Authentication = () => {
-  return <></>;
+  return (
+    <>
+      <main className={"h-[calc(100dvh-96px)] w-full flex justify-center"}>
+        <div className="basis-1/2 border"></div>
+        <div className="basis-1/2 border"></div>
+      </main>
+    </>
+  );
 };
 
 export default Authentication;
