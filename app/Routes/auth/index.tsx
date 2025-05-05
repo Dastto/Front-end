1aa1d19230ed2b015513800b7751b26607018900
tikrack <tikrackcode@gmail.com>
2025-05-05 20:49:23 +0330

    feat(auth): handle auth route

---
+++ app/Routes/auth/index.tsx
@@ -0,0 +1,14 @@
+import Container from "~/Components/Commans/UiParts/Container";
+import Authentication from "~/Components/Authentication";
+
+const index = () => {
+  return (
+    <>
+      <Container>
+        <Authentication />
+      </Container>
+    </>
+  );
+};
+
+export default index;
--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,15 @@
-import { index, layout, type RouteConfig } from "@react-router/dev/routes";
+import {
+  index,
+  layout,
+  route,
+  type RouteConfig,
+} from "@react-router/dev/routes";
 
 export default [
   layout("./Services/StarterHandling/index.tsx", [
     layout("./Components/Commans/Layouts/MasterLayout.tsx", [
       index("./Routes/index.tsx"),
+      route("auth", "./Routes/auth/index.tsx"),
     ]),
   ]),
 ] satisfies RouteConfig;
