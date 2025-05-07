f67de88d9fc3c93aebf0dfa6799b2f8b455f0f5e
tikrack <tikrackcode@gmail.com>
2025-05-07 13:31:29 +0330

    feat(submit): handle submit btn

--- app/Components/Commans/Forms/SubmitButton.tsx
+++ app/Components/Commans/Forms/SubmitButton.tsx
@@ -2,6 +2,7 @@ const SubmitButton = () => {
   return (
     <>
       <button
+        type="submit"
         className={`h-[60px] w-full custom-hover cursor-pointer bg-black text-white rounded-full text-xl mt-5 flex justify-center items-center`}
       >
         بزن بریم
