9e87bef041ed27c8cde6e759fc2b63c70d56d3fd
tikrack <tikrackcode@gmail.com>
2025-05-29 21:05:47 +0330

    feat(add): handle add widget

--- app/Components/Template/Widgets.tsx
+++ app/Components/Template/Widgets.tsx
@@ -1,92 +1,63 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Responsive, WidthProvider } from "react-grid-layout";
 import "/node_modules/react-grid-layout/css/styles.css";
 import "/node_modules/react-resizable/css/styles.css";
 import Widget from "~/Components/Widget";
 import { AnimatePresence, motion } from "framer-motion";
 import useTemplate from "~/Hooks/useTemplate";
+import { FADE_UP } from "~/Services/Setting";
+import type { WidgetType } from "~/Context/TemplateContext";
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 const Widgets = () => {
+  const lgLayout = useRef<any>([]);
   const { template } = useTemplate();
+  const [widgets, setWidgets] = useState<any>();
 
-  const largeLayout = [
-    { i: "dino1", x: 0, y: 0, w: 1, h: 1 },
-    { i: "dino2", x: 0, y: 0, w: 1, h: 1 },
-    {
-      i: "dino3",
-      x: 0,
-      y: 0,
-      w: 1,
-      h: 1,
-    },
-    { i: "music", x: 1, y: 0, w: 1, h: 1 },
-  ];
+  useEffect(() => {
+    lgLayout.current = template?.widgets?.map((widget: any, index: number) => {
+      const positions = widget.widget_position;
+      return {
+        i: widget?.widget?.name + index,
+        x: positions.x,
+        y: positions.y,
+        h: parseInt(positions.h),
+        w: parseInt(positions.w),
+      };
+    });
+    setWidgets(template?.widgets);
+  }, [template]);
 
-  const smallLayout = [
-    { i: "a", x: 0, y: 0, w: 12, h: 2 },
-    { i: "b", x: 0, y: 1, w: 12, h: 2 },
-    {
-      i: "c",
-      x: 0,
-      y: 2,
-      w: 12,
-      h: 2,
-    },
-    { i: "d", x: 0, y: 2, w: 12, h: 2 },
-    { i: "e", x: 0, y: 2, w: 12, h: 2 },
-    { i: "h", x: 0, y: 2, w: 12, h: 2 },
-  ];
+  const smallLayout = [{ i: "music", x: 0, y: 0, w: 1, h: 1 }];
 
   const handleStartDrag = (e: any) => {
-    // e.map((item: any) => {
-    //   if (item.i === "dino") {
-    //     console.log(item);
-    //     toast.success("Dino has been dragged");
-    //   }
-    // });
-    // console.log(e);
-  };
-
-  const fadeUp = {
-    initial: { opacity: 0 },
-    animate: { opacity: 1 },
-    transition: { duration: 0.5 },
+    console.log(e);
   };
 
   return (
     <AnimatePresence>
       <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
+        {...FADE_UP}
         className={"xl:w-[820px] float-end my-[70px] ml-[70px]"}
       >
         <div className={"dir-ltr"}>
           <ResponsiveGridLayout
             className="layout"
-            layouts={{ lg: largeLayout, sm: smallLayout }}
+            layouts={{ lg: lgLayout.current, sm: smallLayout }}
             breakpoints={{ lg: 810, sm: 0 }}
-            cols={{ lg: 4, sm: 12 }}
+            cols={{ lg: 4, sm: 1 }}
             rowHeight={169}
             isResizable={false}
             margin={[48, 40]}
             containerPadding={[0, 0]}
-            onDragStart={handleStartDrag}
+            onDragStop={handleStartDrag}
           >
-            <motion.div {...fadeUp} key={"dino1"}>
-              <Widget name={"dino"} />
-            </motion.div>
-            <motion.div {...fadeUp} key={"dino2"}>
-              <Widget name={"dino"} />
-            </motion.div>
-            <motion.div {...fadeUp} key={"dino3"}>
-              <Widget name={"dino"} />
-            </motion.div>
-            <motion.div {...fadeUp} key={"music"} className={""}>
-              <Widget name={"music"} />
-            </motion.div>
+            {widgets?.map((widget: any, index: number) => (
+              <div {...FADE_UP} key={widget.widget.name + index}>
+                <Widget name={widget.widget.name} />
+              </div>
+            ))}
           </ResponsiveGridLayout>
         </div>
       </motion.div>
--- app/Components/Widget/Dino/DinoAction.tsx
+++ app/Components/Widget/Dino/DinoAction.tsx
@@ -1,12 +1,26 @@
 import React from "react";
+import POST from "~/Services/Axios/Methods/POST";
+import { useNavigate } from "react-router";
 
 const DinoAction = () => {
+  const navigate = useNavigate();
+
+  const handleAdd = async () => {
+    const response = await POST("/template/widget", {
+      name: "dino",
+    });
+
+    if (response.status === 200) {
+    }
+  };
+
   return (
     <>
       <button
         className={
           "cursor-pointer active:scale-95 transition-all duration-100 rounded-lg overflow-hidden"
         }
+        onClick={handleAdd}
       >
         <img
           src="/Images/actionBar/dino.png"
