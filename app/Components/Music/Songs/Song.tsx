dfcc10b514f3b61fc72c132e906bb1dcfc10374b
tikrack <tikrackcode@gmail.com>
2025-06-05 00:03:57 +0330

    feat(handle): handle thumbnail

--- app/Components/Music/Songs/Song.tsx
+++ app/Components/Music/Songs/Song.tsx
@@ -1,25 +1,32 @@
 import { Link } from "react-router";
 import React from "react";
+import { FILE_BASE_URL } from "~/Services/Setting";
 
 interface SongPropsTypes {
-  song: {
-    thumbnail: string;
-    duration: string;
-    name: string;
-    slug: string;
-    singer: string;
-    song: string;
-  };
+  song: any;
 }
 
 const Song: React.FC<SongPropsTypes> = ({ song }) => {
+  console.log(song);
+
   return (
-    <>
+    <div data-aos="fade-up">
       <Link
-        className={"h-[330px] bg-gray-100 rounded-[30px] p-3 block"}
-        to={`/${song.slug}`}
-      ></Link>
-    </>
+        className={
+          "h-[330px] bg-white rounded-[30px] p-3 block hover:-translate-y-[10px] transition-all duration-300 ease-in-out"
+        }
+        to={`/music/${song?.slug}`}
+      >
+        <div
+          className="box-top w-full h-[230px] rounded-[20px]"
+          style={{
+            backgroundImage: `url(${FILE_BASE_URL + song?.thumbnail})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+        ></div>
+      </Link>
+    </div>
   );
 };
 
--- app/Components/Music/Songs/index.tsx
+++ app/Components/Music/Songs/index.tsx
@@ -1,17 +1,28 @@
 import Song from "~/Components/Music/Songs/Song";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import GET from "~/Services/Axios/Methods/GET";
 
 const Songs = () => {
-  const fetchSongs = async () => {
-    const response = await GET("/songs", {}, "pagination");
+  const [page, setPage] = useState(1);
+  const [songs, setSongs] = useState([]);
 
-    console.log(response.data);
+  const fetchSongs = async () => {
+    const response = await GET(
+      "/songs",
+      {
+        page: page,
+      },
+      "pagination",
+    );
+
+    if (response.status === 200) {
+      setSongs(response.data?.items);
+    }
   };
 
   useEffect(() => {
     fetchSongs();
-  }, []);
+  }, [page]);
 
   return (
     <>
@@ -20,7 +31,11 @@ const Songs = () => {
           <span className={"text-sm text-dastto"}>Songs</span>
           آهنگ ها
         </h2>
-        <div className="grid grid-cols-5 gap-5 mt-10"></div>
+        <div className="grid grid-cols-5 gap-5 mt-10">
+          {songs.map((song: any, index) => (
+            <Song key={index} song={song} />
+          ))}
+        </div>
       </section>
     </>
   );
