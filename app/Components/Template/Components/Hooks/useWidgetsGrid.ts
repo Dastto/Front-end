29d17b494e04190a1e250cee1b74f5cff9e546bf
tikrack <tikrackcode@gmail.com>
2025-08-05 23:16:16 +0330

    bug(widget): bug in widgets

--- app/Components/Template/Components/Hooks/useWidgetsGrid.ts
+++ app/Components/Template/Components/Hooks/useWidgetsGrid.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import useTemplate from "~/Hooks/useTemplate";
 import useWidget from "~/Hooks/useWidget";
 import POST from "~/Services/Axios/Methods/POST";
@@ -6,12 +6,13 @@ import toast from "react-hot-toast";
 import { ToastSetting } from "~/Services/Setting";
 
 const useWidgetsGrid = () => {
-  const lgLayout = useRef<any>([]);
-  const { template } = useTemplate();
   const { widgets } = useWidget();
+  const [layouts, setLayouts] = useState({ lg: [], sm: [] });
 
   useEffect(() => {
-    lgLayout.current = widgets?.map((widget: any) => {
+    if (!widgets || widgets.length === 0) return;
+
+    const ly = widgets.map((widget: any) => {
       const positions = widget.widget_position;
       return {
         i: "uw-" + widget.id,
@@ -22,24 +23,22 @@ const useWidgetsGrid = () => {
       };
     });
 
-    console.log(lgLayout.current);
-  }, [template]);
-
-  const smallLayout = [{ i: "music", x: 0, y: 0, w: 1, h: 1 }];
+    // @ts-ignore
+    setLayouts({ lg: ly, sm: ly });
+  }, [widgets]);
 
   const handleLayoutChange = async (e: any) => {
-    const response = await POST("/template/widgets/arrangements", {
-      layout: e,
-    });
-
-    if (response.status !== 200) {
-      toast.error("error", ToastSetting);
-    }
+    // const response = await POST("/template/widgets/arrangements", {
+    //   layout: e,
+    // });
+    //
+    // if (response.status !== 200) {
+    //   toast.error("error", ToastSetting);
+    // }
   };
 
   return {
-    lgLayout,
-    smallLayout,
+    layouts,
     widgets,
     handleLayoutChange,
   };
